594d48b57954879daa81066517f84493e8a8eee1
smruti rekha pradhan <136621177+smruti-hustles@users.noreply.github.com>
2024-02-08 00:07:50 +0530

    Create EndingWithGivenWord.py

---
+++ EndingWithGivenWord.py
@@ -0,0 +1,27 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#take a string
+s=input()
+#take another string
+t=input()
+#reversing first string
+s1=s[::-1]
+print(s1)
+t1=s1[:len(t)]
+t2=t1
+print(t1)
+print(t2)
+if t1==t2:
+    print("yes")
+else:
+    print("no")
+
+    
+        
+
