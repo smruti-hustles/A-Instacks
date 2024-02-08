555e3a97a4fcfe915b790c2f6d7d72c1cfb85158
smruti rekha pradhan <136621177+smruti-hustles@users.noreply.github.com>
2024-02-08 21:47:20 +0530

    Update FindFrequencyMain.java

--- FindFrequencyMain.java
+++ FindFrequencyMain.java
@@ -1,13 +1,11 @@
 /******************************************************************************
 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
+                      Frequency of given character
 
 *******************************************************************************/
 import java.util.*;
 
-public class Main {
+public class FindFrequencyMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
