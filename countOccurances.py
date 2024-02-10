1a66b74dbed36700597ccc4b6e756ba2a856bf47
smruti rekha pradhan <136621177+smruti-hustles@users.noreply.github.com>
2024-02-10 19:07:38 +0530

    Create countOccurances.py

---
+++ countOccurances.py
@@ -0,0 +1,11 @@
+'''
+
+Write a Python program to find all occurrences of the sub-string “USA” in given string ignoring the case
+
+If the sub-string is not present return the count as 0.
+
+'''
+a=input()
+b=a.lower()
+print(b)
+print (b.count("usa"))
